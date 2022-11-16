2fa47760061a1a4d441e31ad5e8fac8ced10a9bb
Paula Mayumi <62121416+mayumisiano@users.noreply.github.com>
2022-11-16 17:59:00 -0300

    Placeholders + campos foto/deficiencia

--- src/paginas/cadastrarUsuario/CadastrarUsuario.tsx
+++ src/paginas/cadastrarUsuario/CadastrarUsuario.tsx
@@ -75,82 +75,43 @@ function CadastroUsuario() {
     }
 
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-      <Grid item xs={6} className="imagem2"></Grid>
-      <Grid item xs={6} alignItems="center">
-        <Box paddingX={10}>
-          <form onSubmit={onSubmit}>
-            <Typography
-              variant="h3"
-              gutterBottom
-              color="textPrimary"
-              component="h3"
-              align="center"
-              className="textos2"
-            >
-              Cadastrar
-            </Typography>
-            <TextField
-              value={user.nome}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-              id="nome"
-              label="nome"
-              variant="outlined"
-              name="nome"
-              margin="normal"
-              fullWidth
-            />
-            <TextField
-              value={user.usuario}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-              id="usuario"
-              label="usuario"
-              variant="outlined"
-              name="usuario"
-              margin="normal"
-              fullWidth
-            />
-            <TextField
-              value={user.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-              id="senha"
-              label="senha"
-              variant="outlined"
-              name="senha"
-              margin="normal"
-              type="password"
-              fullWidth
-            />
-            <TextField
-              value={confirmarSenha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                confirmarSenhaHandle(e)
-              }
-              id="confirmarSenha"
-              label="confirmarSenha"
-              variant="outlined"
-              name="confirmarSenha"
-              margin="normal"
-              type="password"
-              fullWidth
-            />
-            <Box marginTop={2} textAlign="center">
-              <Link to="/login" className="text-decorator-none">
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  className="btnCancelar">               
-                  Cancelar
-                </Button>
-              </Link>
-              <Button type="submit" variant="contained" color="primary">
-                Cadastrar
-              </Button>
-            </Box>
-          </form>
-        </Box>
-      </Grid>
-    </Grid>
+     <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+
+                <Box paddingX={10}>
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastrar</Typography>
+
+                        <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='nome' variant='outlined' name='nome' margin='normal' required fullWidth />
+
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' placeholder='Digite um e-mail válido' variant='outlined' name='usuario' margin='normal' required fullWidth />
+
+                        <TextField value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='foto' label='foto' placeholder='Insira a URL da sua foto' variant='outlined' name='foto' margin='normal' fullWidth />
+
+                        <TextField value={user.deficiencia} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='deficiencia' label='deficiencia' placeholder='Insira qual deficiência você possui' variant='outlined' name='deficiencia' margin='normal' fullWidth />
+
+                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' placeholder='Digite sua senha' variant='outlined' name='senha' margin='normal' required type='password' fullWidth />
+
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' placeholder='Confirme sua senha' variant='outlined' name='confirmarSenha' margin='normal' required type='password' fullWidth />
+
+                        <Box marginTop={2} textAlign='center'>
+
+                            <Link to='/login' className='text-decorator-none'>
+                                <Button variant='contained' color='secondary' className='btnCancelar'>
+                                    Cancelar
+                                </Button>
+                            </Link>
+
+                            <Button type='submit' variant='contained' color='primary'>
+                                Cadastrar
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+            </Grid>
+
+        </Grid>
   );
 }
 
